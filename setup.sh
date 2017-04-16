528476efce82da62f5242af5be7ce5f529916004
Raja. Soundaramourty <rajasoun@icloud.com>
2017-04-16 10:31:24 +0530

    COG - SLACK Setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
-curl -o docker-compose.yml \
-  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.yml
+#curl -o docker-compose.yml \
+#  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.yml
 
-curl -o docker-compose.common.yml \
-  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.common.yml
+#curl -o docker-compose.common.yml \
+#  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.common.yml
 
-curl -o docker-compose.override.yml \
-  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.override.slack_example.yml
+#curl -o docker-compose.override.yml \
+#  https://raw.githubusercontent.com/operable/cog/1.0.0/docker-compose.override.slack_example.yml
+
+docker-compose exec cog bash -c 'cogctl user create rajasoun  --first-name="Raja" --last-name="Soundaramourty" --email="rajasoun@icloud.com" --password="secret"'
+docker-compose exec cog bash -c 'cogctl chat-handle create rajasoun slack rajasoun'
